3fe0b224f52ae774bf19aeb274e1935c26dd0980
techskicompany <techskicompany@gmail.com>
2025-06-29 13:23:19 +0100

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,8 +1,5 @@
 
 const mysql=require('mysql2/promise');
-const { connectOptions } = require('./use_mqtt.js');
-const mqtt = require('mqtt');
-
 
 
 const dbConfig={
@@ -14,29 +11,5 @@ const dbConfig={
 const pool = mysql.createPool(dbConfig);
 
 
-/**
- * MQTT Broker options
- */
-const clientId = 'tech_ski_tracking_' + Math.random().toString(16).substring(2, 8)
-const options = {
-clientId,
-clean: true,
-connectTimeout: 4000,
-username: 'emqx_test',
-password: 'emqx_test',
-reconnectPeriod: 1000,
-}
-
-const { protocol, host, port } = connectOptions
-
-let connectUrl = `${protocol}://${host}:${port}`
-if (['ws', 'wss'].includes(protocol)) {
-connectUrl += '/mqtt'
-}
-/**
- * MQTT Broker connection
- */
-const client = mqtt.connect(connectUrl, options)
-
  
-module.exports ={pool,client};
+module.exports ={pool};
