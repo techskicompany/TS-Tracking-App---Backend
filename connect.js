c5ef3ea006d9575082186b2788c105fb4dc25975
techskicompany <techskicompany@gmail.com>
2025-07-07 09:31:56 +0100

    Update connect.js

--- connect.js
+++ connect.js
@@ -4,9 +4,9 @@ const mysql=require('mysql2/promise');
 
 const dbConfig={
     host:'sql3.freesqldatabase.com',
-    user:'sql3787306',
+    user:'sql3788708',
     password:'YLq5zEs9NP',
-    database:'sql3787306'
+    database:'sql3788708'
 }
 const pool = mysql.createPool(dbConfig);
 
